501e16311b1f4edcce36f7c56d354ade96c1358e
Antonino Praxedes <antonino.praxedes@gmail.com>
2022-03-06 23:55:04 -0400

    feat: adding text edit controller (as draft)

--- lib/app/modules/auth/register/register_controller.dart
+++ lib/app/modules/auth/register/register_controller.dart
@@ -1,10 +1,23 @@
+import 'package:delicias_burger_mobile/app/core/mixins/loader_mixin.dart';
+import 'package:delicias_burger_mobile/app/core/mixins/messages_mixin.dart';
 import 'package:delicias_burger_mobile/app/repositories/auth/auth_repository.dart';
 import 'package:get/get.dart';
 
-class RegisterController extends GetxController {
+class RegisterController extends GetxController
+    with LoaderMixin, MessagesMixin {
   final AuthRepository _authRepository;
+
+  final loading = false.obs;
+  final message = Rxn<MessageModel>();
 
   RegisterController({
     required AuthRepository authRepository,
   }) : _authRepository = authRepository;
+
+  @override
+  void onInit() {
+    loaderListener(loading);
+    messageListener(message);
+    super.onInit();
+  }
 }
